b4e96f2a8b24020debc74afff7b1caea53f3a179
Shane Xu <xusheng0711@gmail.com>
2022-11-10 08:29:37 +0800

    width

--- scripts/olybarp.sh
+++ scripts/olybarp.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ "$HOST" = "shanes-archlaptop" ]; then
+    export TOPRIGHT_WIDTH='15%'
+    export TOPRIGHT_OFFSET_X='85%'
+    export TOPBAR_WIDTH='85%'
+else
+    export TOPRIGHT_WIDTH='11.3%'
+    export TOPRIGHT_OFFSET_X='88.7%'
+    export TOPBAR_WIDTH='88.7%'
+fi
+
 pgrep polybar | xargs kill -9
 ps aux | grep '[g]et_spotify' | awk '{print $2}' | xargs kill -9
 ps aux | grep '[p]olywins' | awk '{print $2}' | xargs kill
