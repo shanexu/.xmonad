98022fd76566eed6f0e1dd7400d87c8c4fef924d
Shane Xu <xusheng0711@gmail.com>
2023-03-03 00:01:03 +0800

    reformat

--- xmonad.hs
+++ xmonad.hs
@@ -1,67 +1,38 @@
-import XMonad
-    ( controlMask,
-      mod1Mask,
-      mod4Mask,
-      shiftMask,
-      xK_0,
-      xK_F2,
-      xK_backslash,
-      xK_a,
-      xK_c,
-      xK_v,
-      xK_e,
-      xK_g,
-      xK_l,
-      xK_m,
-      xK_p,
-      xK_r,
-      xK_w,
-      xK_x,
-      spawn,
-      whenJust,
-      (|||),
-      xmonad,
-      (-->),
-      (<+>),
-      (=?),
-      className,
-      composeAll,
-      doFloat,
-      doIgnore,
-      doF,
-      title,
-      resource,
-      (<&&>),
-      screenWorkspace,
-      windows,
-      withFocused,
-      (.|.),
-      XConfig(focusFollowsMouse, modMask, terminal, workspaces, borderWidth,
-              normalBorderColor, focusedBorderColor, handleEventHook, manageHook,
-              layoutHook, logHook, startupHook), sendMessage, ifM )
-import XMonad.Config.Gnome
-import XMonad.Config.Desktop
-import XMonad.Hooks.DynamicLog
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Maximize
-import XMonad.Layout.Spacing
-import XMonad.Layout.ThreeColumns
-import XMonad.Util.SpawnOnce
-import XMonad.Util.EZConfig
-import XMonad.Hooks.SetWMName
-import XMonad.Hooks.ManageHelpers ( doFullFloat, isFullscreen, doCenterFloat )
-import XMonad.Hooks.EwmhDesktops
-import Network.HostName
-import qualified XMonad.StackSet as W
-import XMonad.Actions.NoBorders
-import System.Directory
-import qualified XMonad.DBus as D
-import qualified DBus.Client as DC
-import XMonad.Util.Themes ( ThemeInfo(theme) )
-import qualified Data.Text as T
-import Data.Text.Encoding.Error ( ignore )
-import XMonad.Actions.CopyWindow ( copyToAll )
-import Control.Monad (when)
+import           Control.Monad              (when)
+import qualified Data.Text                  as T
+import           Data.Text.Encoding.Error   (ignore)
+import qualified DBus.Client                as DC
+import           Network.HostName
+import           System.Directory
+import           XMonad                     (XConfig (borderWidth, focusFollowsMouse, focusedBorderColor, handleEventHook, layoutHook, logHook, manageHook, modMask, normalBorderColor, startupHook, terminal, workspaces),
+                                             className, composeAll, controlMask,
+                                             doF, doFloat, doIgnore, ifM,
+                                             mod1Mask, mod4Mask, resource,
+                                             screenWorkspace, sendMessage,
+                                             shiftMask, spawn, title, whenJust,
+                                             windows, withFocused, xK_0, xK_F2,
+                                             xK_a, xK_backslash, xK_c, xK_e,
+                                             xK_g, xK_l, xK_m, xK_p, xK_r, xK_v,
+                                             xK_w, xK_x, xmonad, (-->), (.|.),
+                                             (<&&>), (<+>), (=?), (|||))
+import           XMonad.Actions.CopyWindow  (copyToAll)
+import           XMonad.Actions.NoBorders
+import           XMonad.Config.Desktop
+import           XMonad.Config.Gnome
+import qualified XMonad.DBus                as D
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.EwmhDesktops
+import           XMonad.Hooks.ManageHelpers (doCenterFloat, doFullFloat,
+                                             isFullscreen)
+import           XMonad.Hooks.SetWMName
+import           XMonad.Layout.Maximize
+import           XMonad.Layout.NoBorders
+import           XMonad.Layout.Spacing
+import           XMonad.Layout.ThreeColumns
+import qualified XMonad.StackSet            as W
+import           XMonad.Util.EZConfig
+import           XMonad.Util.SpawnOnce
+import           XMonad.Util.Themes         (ThemeInfo (theme))
 
 main :: IO ()
 main = do
