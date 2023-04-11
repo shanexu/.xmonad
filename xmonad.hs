01a06178649bd5a351c915f87ac04d7ed251f5b8
Shane Xu <xusheng0711@gmail.com>
2023-04-12 07:37:24 +0800

    reformat and add bsp

--- xmonad.hs
+++ xmonad.hs
@@ -1,56 +1,98 @@
-import           Control.Monad                      (when)
-import qualified Data.Text                          as T
-import           Data.Text.Encoding.Error           (ignore)
-import qualified DBus.Client                        as DC
-import           Network.HostName
-import           System.Directory
-import           XMonad                             (XConfig (borderWidth, focusFollowsMouse, focusedBorderColor, handleEventHook, layoutHook, logHook, manageHook, modMask, normalBorderColor, startupHook, terminal, workspaces),
-                                                     appName, className,
-                                                     composeAll, controlMask,
-                                                     doF, doFloat, doIgnore,
-                                                     ifM, mod1Mask, mod4Mask,
-                                                     resource, screenWorkspace,
-                                                     sendMessage, shiftMask,
-                                                     spawn, stringProperty,
-                                                     title, whenJust, windows,
-                                                     withFocused, xK_0, xK_Down,
-                                                     xK_F2, xK_Left, xK_Right,
-                                                     xK_Up, xK_a, xK_backslash,
-                                                     xK_c, xK_e, xK_g, xK_l,
-                                                     xK_m, xK_p, xK_r, xK_t,
-                                                     xK_v, xK_w, xK_x, xK_z,
-                                                     xmonad, (-->), (.|.),
-                                                     (<&&>), (<+>), (=?), (|||))
-import           XMonad.Actions.CopyWindow          (copyToAll)
-import           XMonad.Actions.CycleWS             (Direction1D (Next, Prev))
-import           XMonad.Actions.Navigation2D
-import           XMonad.Actions.NoBorders
-import           XMonad.Config.Desktop
-import           XMonad.Config.Gnome
-import qualified XMonad.DBus                        as D
-import           XMonad.Hooks.DynamicLog
-import           XMonad.Hooks.EwmhDesktops
-import           XMonad.Hooks.ManageHelpers         (doCenterFloat, doFullFloat,
-                                                     isFullscreen)
-import           XMonad.Hooks.Place                 (smart)
-import           XMonad.Hooks.SetWMName
-import           XMonad.Layout.BinarySpacePartition
-import           XMonad.Layout.BorderResize
-import           XMonad.Layout.Maximize
-import           XMonad.Layout.NoBorders
-import           XMonad.Layout.Spacing
-import           XMonad.Layout.Tabbed               (tabbed)
-import           XMonad.Layout.ThreeColumns
-import qualified XMonad.StackSet                    as W
-import           XMonad.Util.EZConfig
-import           XMonad.Util.NamedScratchpad        (NamedScratchpad (NS),
-                                                     customFloating,
-                                                     namedScratchpadAction,
-                                                     namedScratchpadManageHook,
-                                                     scratchpadWorkspaceTag)
-import           XMonad.Util.SpawnOnce
-import           XMonad.Util.Themes                 (ThemeInfo (theme))
-import           XMonad.Util.WorkspaceCompare       (filterOutWs)
+import Control.Monad (when)
+import DBus.Client qualified as DC
+import Data.Text qualified as T
+import Data.Text.Encoding.Error (ignore)
+import Network.HostName
+import System.Directory
+import XMonad
+  ( XConfig (borderWidth, focusFollowsMouse, focusedBorderColor, handleEventHook, layoutHook, logHook, manageHook, modMask, normalBorderColor, startupHook, terminal, workspaces),
+    appName,
+    className,
+    composeAll,
+    controlMask,
+    doF,
+    doFloat,
+    doIgnore,
+    ifM,
+    mod1Mask,
+    mod4Mask,
+    resource,
+    screenWorkspace,
+    sendMessage,
+    shiftMask,
+    spawn,
+    stringProperty,
+    title,
+    whenJust,
+    windows,
+    withFocused,
+    xK_0,
+    xK_Down,
+    xK_F2,
+    xK_Left,
+    xK_Right,
+    xK_Up,
+    xK_a,
+    xK_backslash,
+    xK_c,
+    xK_e,
+    xK_g,
+    xK_h,
+    xK_j,
+    xK_k,
+    xK_l,
+    xK_m,
+    xK_p,
+    xK_r,
+    xK_s,
+    xK_t,
+    xK_v,
+    xK_w,
+    xK_x,
+    xK_z,
+    xmonad,
+    (-->),
+    (.|.),
+    (<&&>),
+    (<+>),
+    (=?),
+    (|||),
+  )
+import XMonad.Actions.CopyWindow (copyToAll)
+import XMonad.Actions.CycleWS (Direction1D (Next, Prev))
+import XMonad.Actions.Navigation2D
+import XMonad.Actions.NoBorders
+import XMonad.Config.Desktop
+import XMonad.Config.Gnome
+import XMonad.DBus qualified as D
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageHelpers
+  ( doCenterFloat,
+    doFullFloat,
+    isFullscreen,
+  )
+import XMonad.Hooks.Place (smart)
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.BorderResize
+import XMonad.Layout.Maximize
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
+import XMonad.Layout.Tabbed (tabbed)
+import XMonad.Layout.ThreeColumns
+import XMonad.StackSet qualified as W
+import XMonad.Util.EZConfig
+import XMonad.Util.NamedScratchpad
+  ( NamedScratchpad (NS),
+    customFloating,
+    namedScratchpadAction,
+    namedScratchpadManageHook,
+    scratchpadWorkspaceTag,
+  )
+import XMonad.Util.SpawnOnce
+import XMonad.Util.Themes (ThemeInfo (theme))
+import XMonad.Util.WorkspaceCompare (filterOutWs)
 
 main :: IO ()
 main = do
@@ -58,62 +100,81 @@ main = do
   dbus <- D.connect
   D.requestAccess dbus
   xmonad
-    $ navigation2D def
-                   (xK_Up, xK_Left, xK_Down, xK_Right)
-                   [(mod4Mask,               windowGo  ),
-                     (mod4Mask .|. shiftMask, windowSwap)]
-                   False
-    $ ewmhFullscreen . addEwmhWorkspaceSort (pure (filterOutWs [scratchpadWorkspaceTag])) $ gnomeConfig
-    { modMask = myModMask
-    , logHook = dynamicLogWithPP (myLogHook dbus) <+> logHook gnomeConfig
-    -- , terminal = "env GLFW_IM_MODULE=ibus kitty"
-    -- , terminal = "wezterm"
-    , terminal = "tabbed -c alacritty --embed"
-    , workspaces = myWorkspaces
-    , borderWidth = 6
-    , focusFollowsMouse = True
-    , layoutHook = myLayout
-    -- , normalBorderColor = "#707880"
-    -- , focusedBorderColor = "#33AADD"
-    , normalBorderColor = "#555555"
-    , focusedBorderColor = "#f36864"
-    , handleEventHook = handleEventHook gnomeConfig
-    , startupHook = myStartupHook hostname
-    , manageHook = myManageHook <+> manageHook gnomeConfig
-    } `additionalKeys` (
-    [((myModMask, key), windows $ W.greedyView ws) | (key, ws) <- myExtraWorkspaces ]
-    ++ [((myModMask .|. shiftMask, key), windows $ W.shift ws) | (key, ws) <- myExtraWorkspaces]
-    ++ [((m .|. myModMask, key), screenWorkspace sc >>= flip whenJust (windows . f)) -- Replace 'mod1Mask' with your mod key of choice.
-       | (key, sc) <- zip [xK_w, xK_e, xK_r] [2,0,1] -- was [0..] *** change to match your screen order ***
-       , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
-    ++ [
-        ((myModMask, xK_g), withFocused toggleBorder)
-       ,((myModMask, xK_p), spawn "rofi -combi-modi window,drun,run -show combi -font 'Cascadia Code 14' -icon-theme 'Fluent' -show-icons -dpi 144")
-       ,((myModMask .|. shiftMask, xK_p), spawn "rofi -show window -font 'Cascadia Code 14' -icon-theme 'Fluent' -show-icons -dpi 144")
-       ,((myModMask, xK_x), spawn "QT_AUTO_SCREEN_SCALE_FACTOR=0 flameshot full -c")
-       ,((myModMask .|. shiftMask, xK_x), spawn "QT_AUTO_SCREEN_SCALE_FACTOR=0 flameshot gui")
-       ,((myModMask .|. shiftMask, xK_v), spawn "copyq toggle")
-       ,((myModMask, xK_backslash), spawn "1password --quick-access")
-       -- ,((mod1Mask .|. controlMask, xK_l), spawn $ if hostname == "archdesktop" then "dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock" else "xlock -mode rain")
-       ,((mod1Mask .|. controlMask, xK_l), spawn $ "dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock")
-       ,((myModMask .|. shiftMask, xK_m), withFocused (sendMessage . maximizeRestore))
-       ,((myModMask, xK_a), spawn "autorandr -c")
-       ,((myModMask .|. controlMask, xK_t), namedScratchpadAction scratchpads "dropDownTerminal")
-       -- ,((myModMask, xK_z), sendMessage Balance)
-       -- ,((myModMask .|. shiftMask, xK_z), sendMessage Equalize)
-       ]
-    )
+    $ navigation2D
+      def
+      (xK_Up, xK_Left, xK_Down, xK_Right)
+      [ (mod4Mask, windowGo),
+        (mod4Mask .|. shiftMask, windowSwap)
+      ]
+      False
+    $ ewmhFullscreen . addEwmhWorkspaceSort (pure (filterOutWs [scratchpadWorkspaceTag]))
+    $ gnomeConfig
+      { modMask = myModMask,
+        logHook = dynamicLogWithPP (myLogHook dbus) <+> logHook gnomeConfig,
+        -- , terminal = "env GLFW_IM_MODULE=ibus kitty"
+        -- , terminal = "wezterm"
+        terminal = "tabbed -c alacritty --embed",
+        workspaces = myWorkspaces,
+        borderWidth = 6,
+        focusFollowsMouse = True,
+        layoutHook = myLayout,
+        -- , normalBorderColor = "#707880"
+        -- , focusedBorderColor = "#33AADD"
+        normalBorderColor = "#555555",
+        focusedBorderColor = "#f36864",
+        handleEventHook = handleEventHook gnomeConfig,
+        startupHook = myStartupHook hostname,
+        manageHook = myManageHook <+> manageHook gnomeConfig
+      }
+      `additionalKeys` ( [((myModMask, key), windows $ W.greedyView ws) | (key, ws) <- myExtraWorkspaces]
+                           ++ [((myModMask .|. shiftMask, key), windows $ W.shift ws) | (key, ws) <- myExtraWorkspaces]
+                           ++ [ ((m .|. myModMask, key), screenWorkspace sc >>= flip whenJust (windows . f)) -- Replace 'mod1Mask' with your mod key of choice.
+                                | (key, sc) <- zip [xK_w, xK_e, xK_r] [2, 0, 1], -- was [0..] *** change to match your screen order ***
+                                  (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+                              ]
+                           ++ [ ((myModMask, xK_g), withFocused toggleBorder),
+                                ((myModMask, xK_p), spawn "rofi -combi-modi window,drun,run -show combi -font 'Cascadia Code 14' -icon-theme 'Fluent' -show-icons -dpi 144"),
+                                ((myModMask .|. shiftMask, xK_p), spawn "rofi -show window -font 'Cascadia Code 14' -icon-theme 'Fluent' -show-icons -dpi 144"),
+                                ((myModMask, xK_x), spawn "QT_AUTO_SCREEN_SCALE_FACTOR=0 flameshot full -c"),
+                                ((myModMask .|. shiftMask, xK_x), spawn "QT_AUTO_SCREEN_SCALE_FACTOR=0 flameshot gui"),
+                                ((myModMask .|. shiftMask, xK_v), spawn "copyq toggle"),
+                                ((myModMask, xK_backslash), spawn "1password --quick-access"),
+                                -- ,((mod1Mask .|. controlMask, xK_l), spawn $ if hostname == "archdesktop" then "dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock" else "xlock -mode rain")
+                                ((mod1Mask .|. controlMask, xK_l), spawn "dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock"),
+                                ((myModMask .|. shiftMask, xK_m), withFocused (sendMessage . maximizeRestore)),
+                                ((myModMask, xK_a), spawn "autorandr -c"),
+                                ((myModMask .|. controlMask, xK_t), namedScratchpadAction scratchpads "dropDownTerminal"),
+                                ((myModMask .|. mod1Mask, xK_l), sendMessage $ ExpandTowards R),
+                                ((myModMask .|. mod1Mask, xK_h), sendMessage $ ExpandTowards L),
+                                ((myModMask .|. mod1Mask, xK_j), sendMessage $ ExpandTowards D),
+                                ((myModMask .|. mod1Mask, xK_k), sendMessage $ ExpandTowards U),
+                                ((myModMask .|. mod1Mask .|. controlMask, xK_l), sendMessage $ ShrinkFrom R),
+                                ((myModMask .|. mod1Mask .|. controlMask, xK_h), sendMessage $ ShrinkFrom L),
+                                ((myModMask .|. mod1Mask .|. controlMask, xK_j), sendMessage $ ShrinkFrom D),
+                                ((myModMask .|. mod1Mask .|. controlMask, xK_k), sendMessage $ ShrinkFrom U),
+                                ((myModMask, xK_r), sendMessage Rotate),
+                                ((myModMask, xK_s), sendMessage Swap),
+                                ((myModMask .|. mod1Mask, xK_p), sendMessage FocusParent),
+                                ((myModMask, xK_z), sendMessage Balance),
+                                ((myModMask .|. shiftMask, xK_z), sendMessage Equalize)
+                              ]
+                       )
 
-myLogHook dbus = def { ppOutput =
-                       \log -> do
-                         let delimiter = T.pack " : "
-                             (_, tail) = T.breakOn delimiter (T.pack log)
-                             layout = drop 12
-                               $ T.unpack tail
-                         D.send dbus layout }
+myLogHook dbus =
+  def
+    { ppOutput =
+        \log -> do
+          let delimiter = T.pack " : "
+              (_, tail) = T.breakOn delimiter (T.pack log)
+              layout =
+                drop 20 $
+                  T.unpack tail
+          D.send dbus layout
+    }
 
--- myLayout = smartSpacing 4 $ smartBorders $ maximize $ borderResize $ layoutHook gnomeConfig ||| desktopLayoutModifiers (ThreeColMid 1 (3/100) (1/2) ||| emptyBSP)
-myLayout = smartBorders $ maximize $ borderResize $ layoutHook gnomeConfig ||| desktopLayoutModifiers (ThreeColMid 1 (3/100) (1/2) ||| emptyBSP)
+myLayout = smartBorders $ maximize $ borderResize $ smartSpacing 4 $ layoutHook gnomeConfig ||| desktopLayoutModifiers (ThreeColMid 1 (3 / 100) (1 / 2) ||| emptyBSP)
+
+-- myLayout = smartBorders $ maximize $ borderResize $ layoutHook gnomeConfig ||| desktopLayoutModifiers (ThreeColMid 1 (3/100) (1/2) ||| emptyBSP)
 
 myLauncher = "$($HOME/.cabal/bin/yeganesh -x -- -fn 'Monoid-8' -b)"
 
@@ -125,34 +186,39 @@ myStartupHook hostname = do
   -- spawn "$HOME/.xmonad/scripts/int2t.sh"
   -- spawn "$HOME/bin/redmi"
   -- when (hostname == "shanes-archlaptop") $ spawn "$HOME/.xmonad/scripts/autolockx.sh"
-  spawn "$HOME/.xmonad/scripts/ay-night-switcherd.sh"
-  -- 修复firefox全屏显示
-  setWMName "LG3D"
+  spawn "$HOME/.config/xmonad/scripts/ay-night-switcherd.sh"
 
-scratchpads = [ NS "dropDownTerminal" "tabbed -c -n Drop-Down-Terminal alacritty -o window.opacity=0.80 --embed" (appName =? "Drop-Down-Terminal") (customFloating $ W.RationalRect (1/8) (0/6) (3/4) (2/3))
-              ]
-  where role = stringProperty "WM_WINDOW_ROLE"
+-- setWMName "LG3D"
 
-myManageHook = namedScratchpadManageHook scratchpads <+> composeAll
-  [ className =? "mpv" --> doFloat
-  , className =? "MComix" --> doFloat
-  , className =? "Gnome-mpv" --> doFloat
-  , className =? "Gnome-tweaks" --> doFloat
-  , className =? "Gnome-control-center" --> doFloat
-  , className =? "EasyConnect" --> doFloat
-  , className =? "Ulauncher" --> hasBorder False >> doFloat
-  , className =? "Gnome-screenshot" --> doFloat
-  , className =? "1Password" --> doFloat
-  , className =? "Youdao Dict" --> hasBorder False >> doFloat
-  , className =? "copyq" --> doCenterFloat
-  , title =? "Quick Access — 1Password" --> doFloat
-  , title =? "Run Application" --> doFloat
-  , title =? "Log Out" --> doFloat
-  , title =? "歌词" --> hasBorder False >> doF W.focusDown <+> doF copyToAll
-  , isFullscreen --> doFullFloat
+scratchpads =
+  [ NS "dropDownTerminal" "tabbed -c -n Drop-Down-Terminal alacritty -o window.opacity=0.80 --embed" (appName =? "Drop-Down-Terminal") (customFloating $ W.RationalRect (1 / 8) (0 / 6) (3 / 4) (2 / 3))
   ]
+  where
+    role = stringProperty "WM_WINDOW_ROLE"
+
+myManageHook =
+  namedScratchpadManageHook scratchpads
+    <+> composeAll
+      [ className =? "mpv" --> doFloat,
+        className =? "MComix" --> doFloat,
+        className =? "Gnome-mpv" --> doFloat,
+        className =? "Gnome-tweaks" --> doFloat,
+        className =? "Gnome-control-center" --> doFloat,
+        className =? "EasyConnect" --> doFloat,
+        className =? "Ulauncher" --> hasBorder False >> doFloat,
+        className =? "Gnome-screenshot" --> doFloat,
+        className =? "1Password" --> doFloat,
+        className =? "Youdao Dict" --> hasBorder False >> doFloat,
+        className =? "copyq" --> doCenterFloat,
+        title =? "Quick Access — 1Password" --> doFloat,
+        title =? "Run Application" --> doFloat,
+        title =? "Log Out" --> doFloat,
+        title =? "歌词" --> hasBorder False >> doF W.focusDown <+> doF copyToAll,
+        isFullscreen --> doFullFloat
+      ]
 
 myWorkspaces = miscs 9 ++ ["0"]
-  where miscs = map (("" ++) . show) . flip take [1..]
+  where
+    miscs = map (("" ++) . show) . flip take [1 ..]
 
 myExtraWorkspaces = [(xK_0, "0")]
