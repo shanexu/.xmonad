97f9ed821a9d8d0ea7af44e4c0f11b070297b35a
Shane Xu <xusheng0711@gmail.com>
2023-05-16 23:33:33 +0800

    find screen resolution then start bar

--- scripts/bars.sh
+++ scripts/bars.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 pkill polybar
-ps aux | grep '[g]et_spotify' | awk '{print $2}' | xargs kill -9
-ps aux | grep '[p]olywins' | awk '{print $2}' | xargs kill -9
+pkill stalonetray
 
-sleep 5
-polybar -r topbar &
+while true; do
+    xrandr --current | grep '5120x2880' && break
+    sleep 1
+done
 
 sleep 1
-
-pkill stalonetray
+polybar -r topbar &
 stalonetray &
+sleep 1
+copyq&
