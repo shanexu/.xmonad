cc0551bcc8e399bb330a5f4340f6079a64e26969
Shane Xu <xusheng0711@gmail.com>
2023-10-04 16:03:10 +0800

    update bars

--- scripts/bars.sh
+++ scripts/bars.sh
@@ -3,13 +3,23 @@
 pkill polybar
 pkill stalonetray
 
+autorandr -c
+
 while true; do
-    xrandr --current | grep '5120x2880' && break
-    sleep 1
+    output=$(xrandr --current)
+    if echo "$output" | grep -q 'DP-4 connected primary 3413x2133+0+0 (normal left inverted right x axis y axis) 345mm x 215mm'
+    then
+        command=$'polybar -r topbar &\nstalonetray --geometry 1x1+2850+0 &\n'
+        break
+    elif echo "$output"  | grep -q '5120x2880'
+    then
+        command=$'polybar -r topbar &\nstalonetray --geometry 1x1+2850+0 &\n'
+        break
+    else
+        sleep 1
+    fi
 done
 
 sleep 1
-polybar -r topbar &
-stalonetray &
-sleep 1
-copyq&
+
+eval $command
