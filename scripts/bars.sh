d769700c515116c5181b3cea2e557ce93f17ebde
Shane Xu <xusheng0711@gmail.com>
2023-11-04 10:11:42 +0800

    bars

--- scripts/bars.sh
+++ scripts/bars.sh
@@ -2,6 +2,7 @@
 
 pkill polybar
 pkill stalonetray
+pkill xfce4-panel
 
 autorandr -c
 
@@ -9,11 +10,18 @@ while true; do
     output=$(xrandr --current)
     if echo "$output" | grep -q 'DP-4 connected primary 3413x2133+0+0 (normal left inverted right x axis y axis) 345mm x 215mm'
     then
-        command=$'polybar -r topbar &\nstalonetray --geometry 1x1+2850+0 &\n'
+        export POLYBAR_DPI=192
+        command=$'polybar -r topbar &\nxfce4-panel &\n'
         break
     elif echo "$output"  | grep -q 'DP-2 connected primary 5120x2880+0+0 (normal left inverted right x axis y axis) 609mm x 349mm'
     then
-        command=$'polybar -r topbar &\nstalonetray --geometry 1x1-535+0 &\n'
+        export POLYBAR_DPI=192
+        command=$'polybar -r topbar &\nxfce4-panel &\n'
+        break
+    elif echo "$output" | grep -q 'eDP-1 connected primary 1920x1080+0+0 (normal left inverted right x axis y axis) 309mm x 174mm'
+    then
+        export POLYBAR_DPI=96
+        command=$'polybar -r topbar &\nxfce4-panel &\n'
         break
     else
         sleep 1
